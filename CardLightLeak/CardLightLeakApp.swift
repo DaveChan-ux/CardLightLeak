e06408c003accfae126068cff47a97a57f97c790
DaveChan-ux <david.chan@rewardstyle.com>
2025-01-19 07:36:06 -0800

    Initial Commit

---
+++ CardLightLeak/CardLightLeakApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardLightLeakApp.swift
+//  CardLightLeak
+//
+//  Created by Dave.Chan on 1/19/25.
+//
+
+import SwiftUI
+
+@main
+struct CardLightLeakApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
